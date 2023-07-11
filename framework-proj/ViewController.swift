1ab1f28572b4d209e1cb30e8a4207754b9c29096
Adina Marginean <adina.marginean@paddypowerbetfair.com>
2023-07-11 19:28:00 +0300

    first commit

---
+++ framework-proj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  framework-proj
+//
+//  Created by Adina Marginean on 7/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ framework-proj/framework_proj.h
@@ -0,0 +1,18 @@
+//
+//  framework_proj.h
+//  framework-proj
+//
+//  Created by Adina Marginean on 7/5/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for framework_proj.
+FOUNDATION_EXPORT double framework_projVersionNumber;
+
+//! Project version string for framework_proj.
+FOUNDATION_EXPORT const unsigned char framework_projVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <framework_proj/PublicHeader.h>
+
+
